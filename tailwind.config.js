e87eb8408063a7792748392f69cd9913b0189ab7
Daniel Buchal <daniel.buchal01@gmail.com>
2024-09-01 00:24:05 +0200

    chore: Color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
+        colors: {
+          theme: {
+            primaryDark: "#1e2944",
+            primaryLight: "#234761",
+            secondary: "#eaeff1"
+          },
+          utility: {
+            selectDate: "#d4ad45",
+            selectTime: "#91c52f"
+          }
+        },
         extend: {},
     },
     plugins: [],
