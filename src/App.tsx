2f09e31120543142c2ef5fdc91224e6ded9e4a1c
Daniel Buchal <daniel.buchal01@gmail.com>
2024-08-31 23:55:07 +0200

    fix: Correct install of tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-
 function App() {
-
-  return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
-  )
+    return (
+        <div className="w-full m-0 p-0 h-screen">
+            <h1>Hello world</h1>
+        </div>
+    );
 }
 
-export default App
+export default App;
